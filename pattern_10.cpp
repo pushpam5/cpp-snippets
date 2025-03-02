33fe329f763d52942cb70108a1f8d0ca6906ec2b
pushpam5 <pushpamsxn58@gmail.com>
2025-03-02 23:22:13 +0530

    first commit

---
+++ pattern_10.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+/*
+Print pattern like below
+
+n = 5
+
+*         
+**        
+***       
+****      
+*****     
+****      
+***       
+**        
+*
+
+*/
+
+void print(int spaces, int stars){
+
+    for(int j = 0; j < stars;j++){
+        cout << "*";
+    }
+
+    for(int j = 0; j < spaces;j++){
+        cout << " ";
+    }
+}
+
+int main() {
+    int n, stars, spaces;
+    cin >> n;
+
+    for(int i = 1; i <= 2 * n - 1; i++) {
+
+        stars = i;
+        if(i > n){
+            stars = 2 * n - i;
+        }
+        spaces = 2 * n - stars;
+        print(spaces, stars);
+        cout << endl;
+
+    }
+    return 0;
+}
